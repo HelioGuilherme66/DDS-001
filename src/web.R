9a81d9e5b0316cb3e674a77ef4b33a10eb01b704
SYNchroACK <synchroack@protonmail.ch>
2018-03-17 20:12:24 +0000

    BUG: fullpath applied

--- src/web.R
+++ src/web.R
@@ -3,7 +3,7 @@
 library(plumber)
 
 # Wrap the model/function/microservice/life
-r<-plumb("randomforest.R")
+r<-plumb("/microservice/randomforest.R")
 
 # Run the server
 r$run(host = '0.0.0.0', port = 80)

d77713fbfedd37f138412f4cb1ea52cd22c0b661
HelioGuilherme66 <helioxentric@gmail.com>
2018-03-17 17:26:22 +0000

    Adds rjason package to requirements.

--- src/requirements.R
+++ src/requirements.R
@@ -1,3 +1,4 @@
 install.packages("randomForest", dependencies = TRUE)
 install.packages("plumber", dependencies = TRUE)
 install.packages("dplyr", dependencies = TRUE)
+install.packages("rjson", dependencies = TRUE)
